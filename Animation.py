2a70fede91a2a6bffe1d1230c4a7c01e54ea887f
erscat <erschcat@gmail.com>
2023-12-03 14:58:24 -0500

    Update Animation.py

--- Animation.py
+++ Animation.py
@@ -24,5 +24,14 @@ def animate(frame_nb):
     ax.clear()
     ax.set_xlabel("X [µm]")
     ax.set_ylabel("Y [µm]")
+    ax.set_title("External field: up = blue, down = red")
     ax.scatter(xpos, ypos,color="black",s=5, zorder=200)
-    
+    plus = valE > 0
+    minus = valE < 0
+    plt.scatter(xposE[plus],0*xposE[plus],color="blue")
+    plt.scatter(xposE[minus],0*xposE[minus],color="red")
+
+print("Starting animation compilation")
+anime = animation.FuncAnimation(fig, animate, frames=len(timeframes), interval=1)
+anime.save("(To rename) Electron dynamics.gif", writer='pillow')
+print("Animation saved successfully")
